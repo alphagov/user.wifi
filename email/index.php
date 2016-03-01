7a33d7eb6d10f211995b05fc721772103294dcf4
AlistairHCowan <alistair@cowanconsulting.co.uk>
2016-03-01 14:26:32 +0000

    added log messages to email handler

--- email/index.php
+++ email/index.php
@@ -1,22 +1,24 @@
 <?php
 
 require ("../common.php");
-// Load the configuration file into a global variable
-loadconfiguration();
-// Connect to the database
-db_connect();
+
 $emailreq = new emailRequest();
-switch ($emailreq->emailToCMD) {
+switch ($emailreq->emailToCMD)
+{
     case "enroll":
+        error_log("Email: Self enrollment request from $emailreq->sender->text");
         $emailreq->enroll();
         break;
     case "sponsor":
+        error_log("Email: Sponsored enrollment request from $emailreq->sender->text");
         $emailreq->sponsor();
         break;
     case "newsite":
+        error_log("Email: New site request request from $emailreq->sender->text");
         $emailreq->newsite();
         break;
     case "logrequest":
+        error_log("Email: Log request from $emailreq->sender->text");
         $emailreq->logrequest();
         break;
 }
