211e8a7dbc7c5c461ec1d0b2ee01417de4d290aa
AlistairHCowan <alistair@cowanconsulting.co.uk>
2016-03-03 10:49:52 +0000

    .

--- email/index.php
+++ email/index.php
@@ -7,19 +7,19 @@ $emailreq = new emailRequest();
 if (preg_match("/^Postmark/",$_SERVER['HTTP_USER_AGENT']))
 {
     $json = file_get_contents('php://input');
-    $data = json_decode(stripslashes($json), true);
+    $data = json_decode(stripslashes($json));
     // Support for Postmark
-    error_log("EMAIL: From : " . $data['From']);
-    $emailreq->setEmailFrom($data['From']);
-    $emailreq->setEmailTo($data['To']);
-    $emailreq->setEmailSubject($data['Subject']);
-    $emailreq->setEmailBody($data['TextBody']);
+    error_log("Postmark EMAIL: From : " . $data->From);
+    $emailreq->setEmailFrom($data->From);
+    $emailreq->setEmailTo($data->To);
+    $emailreq->setEmailSubject($data->Subject);
+    $emailreq->setEmailBody($data->TextBody);
 }
 
 if (isset($_REQUEST['sender']))
 {
     // Support for Mailgun
-    error_log("EMAIL: From : " . $_REQUEST['sender']);
+    error_log("Mailgun EMAIL: From : " . $_REQUEST['sender']);
     $emailreq->setEmailFrom($_REQUEST['sender']);
     $emailreq->setEmailTo($_REQUEST['recipient']);
     $emailreq->setEmailSubject($_REQUEST['subject']);
