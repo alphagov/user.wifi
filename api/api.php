074ce6204b3d63e8d1450166878a48cfda5462d2
AlistairHCowan <alistair@cowanconsulting.co.uk>
2016-03-11 18:38:04 +0000

    .

--- api/api.php
+++ api/api.php
@@ -7,7 +7,7 @@ $aaa = new aaa($_SERVER['SCRIPT_NAME']);
  header("Content-Type: application/json");
  $response['control:Cleartext-Password']= $aaa->user->password;
  $response['reply:Reply-Message']="";
- $json = json_encode($reponse);
+ $json = json_encode($response);
  print $json;
 
 
