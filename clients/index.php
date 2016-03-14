9e6dfdc00d1f948cce9756eef919f5541bdf8c06
AlistairHCowan <alistair@cowanconsulting.co.uk>
2016-03-14 09:30:11 +0000

    .

--- clients/index.php
+++ clients/index.php
@@ -6,15 +6,15 @@ $db = DB::getInstance();
 $dblink = $db->getConnection();
 $handle = $dblink->prepare('select nasname, secret from nas');
 $handle->execute();
-$clientName = str_replace('.','-',$result['nasname']);
-$clientIp = $result['nasname'];
-$clientSecret = $result['secret'];
+
 while ($result = $handle->fetch(PDO::FETCH_ASSOC))
 {
-    print 'client '. $clientName . ' {
+    $clientName = str_replace('.', '-', $result['nasname']);
+    $clientIp = $result['nasname'];
+    $clientSecret = $result['secret'];
+    print 'client ' . $clientName . ' {
         ipaddr = ' . $clientIp . '
-        secret = ' .
-        $clientSecret . '
+        secret = ' . $clientSecret . '
         }
 ';
 }
