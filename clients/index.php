e142068da1c4fd42319d4324b14b2814d1282da5
AlistairHCowan <alistair@cowanconsulting.co.uk>
2016-03-14 11:59:50 +0000

    .

--- clients/index.php
+++ clients/index.php
@@ -1,22 +1,27 @@
 <?php
 
-require ("../common.php");
-
-$db = DB::getInstance();
-$dblink = $db->getConnection();
-$handle = $dblink->prepare('select nasname, secret from nas');
-$handle->execute();
-
-while ($result = $handle->fetch(PDO::FETCH_ASSOC))
+if ($_REQUEST['key'] == "xp93rDXY65DKQ5IiKlUC0sN0WDwj0v")
 {
-    $clientName = str_replace('.', '-', $result['nasname']);
-    $clientIp = $result['nasname'];
-    $clientSecret = $result['secret'];
-    print 'client ' . $clientName . ' {
+
+
+    require ("../common.php");
+
+    $db = DB::getInstance();
+    $dblink = $db->getConnection();
+    $handle = $dblink->prepare('select nasname, secret from nas');
+    $handle->execute();
+
+    while ($result = $handle->fetch(PDO::FETCH_ASSOC))
+    {
+        $clientName = str_replace('.', '-', $result['nasname']);
+        $clientIp = $result['nasname'];
+        $clientSecret = $result['secret'];
+        print 'client ' . $clientName . ' {
         ipaddr = ' . $clientIp . '
         secret = ' . $clientSecret . '
         }
 ';
+    }
 }
 
 ?>
