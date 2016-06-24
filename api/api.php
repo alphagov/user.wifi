d008f66adc555bd163f09272480e87b1976fb3bb
AlistairHCowan <alistair@cowanconsulting.co.uk>
2016-06-24 16:21:33 +0100

    .

--- api/api.php
+++ api/api.php
@@ -5,7 +5,7 @@ require ("../common.php");
 $aaa = new aaa($_SERVER['SCRIPT_NAME']);
 $aaa->requestJson = file_get_contents('php://input');
 $aaa->processRequest();
-header($aaa->responseHeader);
+header($_SERVER["SERVER_PROTOCOL"].' '.$aaa->responseHeader);
 header("Content-Type: application/json");
 
 
