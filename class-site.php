d3329405e1a2a523a47bf8983798c28329cf453d
AlistairHCowan <alistair@cowanconsulting.co.uk>
2016-07-11 15:15:23 +0100

    .

--- class-site.php
+++ class-site.php
@@ -83,8 +83,8 @@ class site
         $updated = FALSE;
         foreach (preg_split("/((\r?\n)|(\r\n?))/", $emailBody) as $line)
         {
-            $line = strreplace(">","",$line);
-            $line = strreplace("*","",$line);
+            $line = str_replace(">","",$line);
+            $line = str_replace("*","",$line);
             $line = trim($line);
             $parameter = strtolower(trim(substr($line, 0, strpos($line,":"))));
             
