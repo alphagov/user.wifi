3b47c8650f95e05845255e3d46fae56e68039af2
AlistairHCowan <alistair@cowanconsulting.co.uk>
2016-09-27 21:55:26 +0100

    SNS subscribe functionality

--- sns/index.php
+++ sns/index.php
@@ -6,32 +6,34 @@ $emailreq = new emailRequest();
 
     $json = file_get_contents('php://input');
     $data = json_decode($json, true);
+    if (isset($data['SubscribeURL']))
+        {
+        file_get_contents($data['SubscribeURL']);
+        }
+        else
+        {
+        error_log("AWS SNS EMAIL: From : " . $data['mail']['commonHeaders']['from'][0]);
+        $emailreq->setEmailFrom($data['mail']['commonHeaders']['from'][0]);
+        $emailreq->setEmailTo($data['mail']['commonHeaders']['to'][0]);
+        $emailreq->setEmailSubject($data['mail']['commonHeaders']['Subject']);
+        $emailreq->setEmailBody($data['Content']);
 
-    error_log("AWS SNS EMAIL: From : " . $data['mail']['commonHeaders']['from'][0]);
-    $emailreq->setEmailFrom($data['mail']['commonHeaders']['from'][0]);
-    $emailreq->setEmailTo($data['mail']['commonHeaders']['to'][0]);
-    $emailreq->setEmailSubject($data['mail']['commonHeaders']['Subject']);
-    $emailreq->setEmailBody($data['Content']);
-
-
-switch ($emailreq->emailToCMD)
-{
-    case "enroll":
-        $emailreq->enroll();
-        break;
-    case "enrol":
-        $emailreq->enroll();
-        break;
-    case "sponsor":
-        $emailreq->sponsor();
-        break;
-    case "newsite":
-        $emailreq->newsite();
-        break;
-    case "logrequest":
-
-        $emailreq->logrequest();
-        break;
-}
+        switch ($emailreq->emailToCMD) {
+            case "enroll":
+                $emailreq->enroll();
+            break;
+            case "enrol":
+                $emailreq->enroll();
+            break;
+            case "sponsor":
+                $emailreq->sponsor();
+            break;
+            case "newsite":
+                $emailreq->newsite();
+            break;
+            case "logrequest":
+                $emailreq->logrequest();
+            break;
+        }
 
 ?>
