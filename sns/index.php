94f5b9b260ce7432145eee09bb556c5c4ed764d3
AlistairHCowan <alistair@cowanconsulting.co.uk>
2016-09-28 10:33:47 +0100

    Filtered raw email address (SNS)

--- sns/index.php
+++ sns/index.php
@@ -13,8 +13,11 @@ if (isset($data['SubscribeURL']))
 } else 
 {
     error_log("AWS SNS EMAIL: From : " . $data['mail']['commonHeaders']['from'][0]);
-    $emailreq->setEmailFrom($data['mail']['commonHeaders']['from'][0]);
-    $emailreq->setEmailTo($data['mail']['commonHeaders']['to'][0]);
+    $pattern = "/([a-zA-Z\.\-]+@[a-zA-Z\.\-]+)/";
+    preg_match_all($pattern,$data['mail']['commonHeaders']['from'][0],$matches);
+    $emailreq->setEmailFrom($matches[0]);
+    preg_match_all($pattern,$data['mail']['commonHeaders']['to'][0],$matches);
+    $emailreq->setEmailTo($matches[0]);
     $emailreq->setEmailSubject($data['mail']['commonHeaders']['subject']);
     $emailreq->setEmailBody($data['content']);
 
